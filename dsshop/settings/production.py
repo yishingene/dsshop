8c86986678ec42b4f4923526457ff68d44f9a7ea
Tim Claes <tim.claes@me.com>
2016-09-08 10:44:45 +0200

    crucial mistake, was connection to the wrong db

--- dsshop/settings/production.py
+++ dsshop/settings/production.py
@@ -1,9 +1,11 @@
 from .settings import *
 
+DEBUG = True
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'festival_db',                      # Or path to database file if using sqlite3.
+        'NAME': 'dsshop_db',                      # Or path to database file if using sqlite3.
         #'NAME': 'test',
         # The following settings are not used with sqlite3:
         'USER': '',
