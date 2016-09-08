f12589af93ad2cd796c2a57e32a72436f2972329
Tim Claes <tim.claes@me.com>
2016-09-08 17:15:42 +0200

    Added supported languages

--- dsshop/settings/settings.py
+++ dsshop/settings/settings.py
@@ -22,6 +22,14 @@ from oscar.defaults import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+from django.utils.translation import ugettext_lazy as _
+
+LANGUAGES = [
+    ('en', _('English')),
+    ('nl', _('Dutch')),
+    ('fr', _('French'))
+]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
