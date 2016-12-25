a8f5214b9df64a75f8179f4910dc73fe3d4e1412
Tim Claes <tim.claes@me.com>
2016-12-25 20:07:24 +0100

    still same

--- myapps/catalogue/management/commands/addimagestoproducts.py
+++ myapps/catalogue/management/commands/addimagestoproducts.py
@@ -39,7 +39,7 @@ class Command(BaseCommand):
 			try:
 				#img = open(image_path, 'r')
 
-				connection = S3Connection(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)
+				connection = S3Connection(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY, host=settings.AWS_S3_HOST)
 
 
 
