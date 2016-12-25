cdd38c34cfd46d8f529ec3f0ded3fa23a220b75d
Tim Claes <tim.claes@me.com>
2016-12-25 20:52:58 +0100

    going to test further in dev mode

--- myapps/catalogue/management/commands/addimagestoproducts.py
+++ myapps/catalogue/management/commands/addimagestoproducts.py
@@ -20,7 +20,8 @@ class Command(BaseCommand):
 		help = 'automatiseer het toevoegen van fotos aan een product'
 
 		if settings.DEV:
-			path = os.path.join(settings.MEDIA_ROOT + '/custom_image_list')
+			#path = os.path.join(settings.MEDIA_ROOT + '/custom_image_list')
+			path = 'https://dsshop.s3.eu-central-1.amazonaws.com/media/custom_image_list'
 
 			for index, product in enumerate(Product.objects.all()):
 
@@ -60,68 +61,36 @@ class Command(BaseCommand):
 
 			self.stdout.write('path: %s' % path)
 
-			from boto.s3.connection import S3Connection
-			from boto.s3.key import Key
-
-			try:
-				connection = S3Connection(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY, host=settings.AWS_S3_HOST)
-
-			except: 
-				self.stdout.write('CONN PROBLEM')
-
-			for index, product in enumerate(Product.objects.all()):
-
-				product_code = product.upc
-				image_path = path + '/' + str(product_code) + '.jpg'
-
-				#self.stdout.write('product_code: %s' % product_code)
-				#self.stdout.write('path: %s' % image_path)
 
 
-				# image exists!
-				#self.stdout.write('CONN OKE!')
-				file_name = str(product_code) + '.jpg'
-
-				bucket = connection.get_bucket('dsshop')
-				bucket_list = bucket.list()
-
-				for item in bucket_list:
-					self.stdout.write('list item: %s' % item)
+			# from boto.s3.connection import S3Connection
+			# from boto.s3.key import Key
 
 
-				#current_file = Object('dsshop', key)
+			# connection = S3Connection(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY, host=settings.AWS_S3_HOST)
+			# bucket = connection.get_bucket('dsshop')
+			# bucket_list = bucket.list()
 
-				#self.stdout.write('KEY: %s' % key)
-				#self.stdout.write('file??? %s' % current_file)
+			# for index, product in enumerate(Product.objects.all()):
+
+			# 	product_code = product.upc
+			# 	image_path = path + '/' + str(product_code) + '.jpg'
+
+			# 	#self.stdout.write('product_code: %s' % product_code)
+			# 	#self.stdout.write('path: %s' % image_path)
 
 
-				# self.stdout.write('path: %s' % image_path)
+			# 	# image exists!
+			# 	#self.stdout.write('CONN OKE!')
+			# 	file_name = str(product_code) + '.jpg'
 
-				# if settings.DEV == True:
-				# 	extended_path = 'file://' + image_path
+			# 	bucket = connection.get_bucket('dsshop')
+			# 	bucket_list = bucket.list()
 
-				# else: 
-				# 	extended_path = image_path
+			# 	for item in bucket_list:
+			# 		self.stdout.write('list item: %s' % item)
 
-				# name = str(product_code) + '.jpg'
-
-				# img_temp = NamedTemporaryFile(delete=True)
-				# img_temp.write(urlopen(extended_path).read())
-				# img_temp.flush()
-
-				# new = ProductImage(product=product)
-
-				# try:
-				# 	new.original.save(name, File(img_temp), True)
-
-				# except: 
-				# 	print('BESTAAT REEDS')
-				# 	continue
-
-				# new.save()
-				# print('SAVING')
-
-			self.stdout.write('--Het is gefixt!--')
+			# self.stdout.write('--Het is gefixt!--')
 
 
 
