795d23541a2042cfa8154f591d7bd3cb6fcd3588
Tim Claes <tim.claes@me.com>
2016-12-26 20:51:57 +0100

    going to test in production again

--- myapps/catalogue/management/commands/addimagestoproducts.py
+++ myapps/catalogue/management/commands/addimagestoproducts.py
@@ -12,6 +12,10 @@ import os
 import urllib
 from urllib.request import urlopen
 
+from psycopg2 import IntegrityError
+
+import requests
+
 
 class Command(BaseCommand):
 
@@ -21,112 +25,74 @@ class Command(BaseCommand):
 
 		if settings.DEV:
 
-			path = 'https://dsshop.s3.eu-central-1.amazonaws.com/media/custom_image_list'
+			'''
+			DIT IS DE AWS METHODE ###################################################################
+			'''
 
-
-			for index, product in enumerate(Product.objects.all()):
-
-				product_code = product.upc
-				file_name = str(product_code) + '.jpg'
-
-				image_path = path + '/' + file_name
-
-				#print('image path: %s' % image_path)
-
-				try:
-					myfile = urlopen(image_path)
-
-				except:
-					self.stdout.write('--ERROR--')
-					continue
-
-				print('product: %s' % product)
-				print('file name: %s' % file_name)
-				print('myfile: %s' % myfile)
-				print('path: %s' % image_path)
-				
-
-				img_temp = NamedTemporaryFile(delete=True)
-				img_temp.write(urlopen(image_path).read())
-				img_temp.flush()
-
-				print('img_temp: %s' % img_temp)
-
-				new_image = ProductImage(product=product)
-
-				try:
-					new_image.original.save(file_name, File(img_temp))
-
-				except: 
-					print('ERROR SAVING IMG')
-					break
-
-				new_image.save()
-				print('SAVING')
-
-			self.stdout.write('--Het is gefixt!--')
-
-			
-			# path = os.path.join(settings.MEDIA_ROOT + '/custom_image_list')
+			# path = 'https://dsshop.s3.eu-central-1.amazonaws.com/media/custom_image_list'
 
 			# for index, product in enumerate(Product.objects.all()):
 
 			# 	product_code = product.upc
-			# 	image_path = path + '/' + str(product_code) + '.jpg'
+			# 	file_name = str(product_code) + '.jpg'
+
+			# 	image_path = path + '/' + file_name
 
 			# 	try:
-			# 		img = open(image_path, 'r')
+			# 		myfile = urlopen(image_path)
 
-			# 	except IOError: 
-			# 		print('IO ERROR')
+			# 	except:
+			# 		self.stdout.write('--ERROR: file bestaat wsl niet--')
 			# 		continue
 
-			# 	# image exists!
-			# 	extended_path = 'file://' + image_path
+			# 	# print('product: %s' % product)
+			# 	# print('file name: %s' % file_name)
+			# 	# print('myfile: %s' % myfile)
+			# 	# print('path: %s' % image_path)
 
-			# 	name = str(product_code) + '.jpg'
+			# 	image_content = ContentFile(requests.get(image_path).content) 
 
-			# 	img_temp = NamedTemporaryFile(delete=True)
-			# 	img_temp.write(urlopen(extended_path).read())
-			# 	img_temp.flush()
-
-			# 	new = ProductImage(product=product)
+			# 	new_image = ProductImage(product=product)
 
 			# 	try:
-			# 		new.original.save(name, File(img_temp), True)
+			# 		#new_image.original.save(file_name, File(img_temp))
+			# 		new_image.original.save(file_name, image_content)
 
-			# 	except: 
+			# 	except IntegrityError: 
+			# 		print('DUPLICATE KEY')
 			# 		continue
 
-			# 	new.save()
+			# 	new_image.save()
 			# 	print('SAVING')
 
 			# self.stdout.write('--Het is gefixt!--')
 
-		else:
+			
+			'''
+			DIT IS DE DEV METHODE ###################################################################
+			'''
 
-			path = 'https://dsshop.s3.eu-central-1.amazonaws.com/media/custom_image_list'
-
+			path = os.path.join(settings.MEDIA_ROOT + '/custom_image_list')
 
 			for index, product in enumerate(Product.objects.all()):
 
 				product_code = product.upc
 				image_path = path + '/' + str(product_code) + '.jpg'
 
-				#print('image path: %s' % image_path)
-
 				try:
-					myfile = urlopen(image_path)
+					img = open(image_path, 'r')
 
-				except:
-					#self.stdout.write('--ERROR--')
+				except IOError: 
+					print('IO ERROR')
 					continue
 
-				#print('myfile: %s' % myfile)
-				file_name = str(product_code) + '.jpg'
+				# image exists!
+				extended_path = 'file://' + image_path
+
+				name = str(product_code) + '.jpg'
 
 				img_temp = NamedTemporaryFile(delete=True)
-				img_temp.write(urlopen(myfile).read())
+				img_temp.write(urlopen(extended_path).read())
 				img_temp.flush()
 
 				new = ProductImage(product=product)
@@ -137,62 +103,41 @@ class Command(BaseCommand):
 				except: 
 					continue
 
-				product.save()
+				new.save()
 				print('SAVING')
 
 			self.stdout.write('--Het is gefixt!--')
 
+		else:
 
-			# from boto.s3.connection import S3Connection
-			# from boto.s3.key import Key
+			path = 'https://dsshop.s3.eu-central-1.amazonaws.com/media/custom_image_list'
 
+			for index, product in enumerate(Product.objects.all()):
 
-			# connection = S3Connection(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY, host=settings.AWS_S3_HOST)
-			# bucket = connection.get_bucket('dsshop')
-			# bucket_list = bucket.list()
+				product_code = product.upc
+				file_name = str(product_code) + '.jpg'
 
-			# for index, product in enumerate(Product.objects.all()):
+				image_path = path + '/' + file_name
 
-			# 	product_code = product.upc
-			# 	image_path = path + '/' + str(product_code) + '.jpg'
+				try:
+					myfile = urlopen(image_path)
 
-			# 	#self.stdout.write('product_code: %s' % product_code)
-			# 	#self.stdout.write('path: %s' % image_path)
+				except:
+					self.stdout.write('--ERROR: file bestaat wsl niet--')
+					continue
 
+				image_content = ContentFile(requests.get(image_path).content) 
 
-			# 	# image exists!
-			# 	#self.stdout.write('CONN OKE!')
-			# 	file_name = str(product_code) + '.jpg'
+				new_image = ProductImage(product=product)
 
-			# 	bucket = connection.get_bucket('dsshop')
-			# 	bucket_list = bucket.list()
+				try:
+					new_image.original.save(file_name, image_content)
 
-			# 	for item in bucket_list:
-			# 		self.stdout.write('list item: %s' % item)
+				except IntegrityError: 
+					print('DUPLICATE KEY')
+					continue
 
-			# self.stdout.write('--Het is gefixt!--')
+				new_image.save()
+				print('SAVING')
 
-
-
-def add_image_to_product(product, product_code):
-
-
-	full_path = path + get_image_file_name(product_code)
-
-	os.path.isfile()
-
-#	result = urllib.urlretrieve(image_url)
-#
-#	product.image.save(
-#		os.path.basename(self.url),
-#		File(open(result[0]))
-#	)
-#
-#	product.save()
-
-def get_image_file_name(product_code):
-
-	filename = str(product_code) + '.jpg'
-
-	return filename
-
+			self.stdout.write('--Het is gefixt!--')
