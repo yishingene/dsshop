4fe569cc5a1c8f962f6a2740e27349c82029eb77
Tim Claes <tim.claes@me.com>
2017-01-02 19:28:39 +0100

    last try before adding 2cv file to static files

--- myapps/catalogue/management/commands/createproducts2cv.py
+++ myapps/catalogue/management/commands/createproducts2cv.py
@@ -21,7 +21,8 @@ IMAGES_PATH = 'https://dsshop.s3.eu-central-1.amazonaws.com/media/custom_image_l
 if settings.DEV == True:
 	IMAGES_PATH = 'file://' + settings.MEDIA_ROOT + '/custom_image_list'
 
-PRODUCTS_FILE = 'https://dsshop.s3.eu-central-1.amazonaws.com/media/2cv_onderdelen.csv'
+#PRODUCTS_FILE = 'https://dsshop.s3.eu-central-1.amazonaws.com/media/2cv_onderdelen.csv'
+PRODUCTS_FILE = 'https://s3.eu-central-1.amazonaws.com/dsshop/media/2cv_onderdelen.csv'
 
 if settings.DEV == True:
 	PRODUCTS_FILE = os.path.join(settings.MEDIA_ROOT, '2cv_onderdelen.csv')
