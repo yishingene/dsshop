81345e1165f5558554d948e56f89fef69de715f3
Tim Claes <tim.claes@me.com>
2017-01-15 19:56:32 +0100

    need to modify all CRUD templates

--- myapps/catalogue/views.py
+++ myapps/catalogue/views.py
@@ -6,9 +6,6 @@ logger = logging.getLogger('oscar.catalogue')
 
 class CatalogueView(OscarCatalogueView):
 
-
 	def get_context_data(self, **kwargs):
-
-		print( '------- MY CUSTOM VIEW context data shit')
 
 		return super(CatalogueView, self).get_context_data(**kwargs)
