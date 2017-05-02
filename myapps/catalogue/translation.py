5d1ff35a5a85a6f7fd26e78744494041910bd8f8
Tim Claes <tim.claes@me.com>
2017-05-02 20:38:47 +0200

    adding translations for categories

--- myapps/catalogue/translation.py
+++ myapps/catalogue/translation.py
@@ -1,9 +1,16 @@
 from modeltranslation.translator import translator, TranslationOptions
-from .models import Product
+from .models import Product, Category
 
+# translations for products
 class ProductTranslationOptions(TranslationOptions):
 
 	fields = ('title', 'description')
 
-
 translator.register(Product, ProductTranslationOptions)
+
+# translations for categories
+class CategoryTranslationOptions(TranslationOptions):
+
+	fields = ('name', 'description')
+
+translator.register(Category, CategoryTranslationOptions)
