d00f463047be78a75855b480801a5b02612dd7be
Tim Claes <tim.claes@me.com>
2017-05-24 21:11:37 +0200

    fixed issue with ajax call on basket template

--- dsshop/settings/settings.py
+++ dsshop/settings/settings.py
@@ -338,8 +338,8 @@ OSCAR_ACCOUNTS_REDIRECT_URL = '/'
 OSCAR_INITIAL_ORDER_STATUS = 'Aanvraag'
 OSCAR_ORDER_STATUS_PIPELINE = {
     'Aanvraag': ('Verzendkosten toegevoegd', 'Afhaling door klant', 'Geannuleerd',),
-    'Verzendkosten toegevoegd': ('Klant verwittigd van verzendkosten'),
-    'Klant verwittigd van verzendkosten': ('Betaald', 'Geannuleerd '),
+    'Verzendkosten toegevoegd': ('Klant verwittigd van verzendkosten', ),
+    'Klant verwittigd van verzendkosten': ('Betaald', 'Geannuleerd', ),
     'Afhaling door klant': ('Betaald','Geannuleerd', ),
     'Betaald': ('Verwerkt'),
     'Verwerkt': (),
