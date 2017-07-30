e059fe8a7a27103e3ac5af139f8d37b1b5723d55
Tim Claes <tim.claes@me.com>
2017-07-30 11:03:20 +0200

    Added some more translations, and implemented mailing functionality when order is placed

--- myapps/checkout/views.py
+++ myapps/checkout/views.py
@@ -1,3 +1,7 @@
+from django.core.mail import EmailMessage
+from django.template import Context
+from django.template.loader import get_template
+
 
 from oscar.core.loading import get_model
 from oscar.apps.checkout.views import PaymentDetailsView as OscarPaymentDetailsView
@@ -5,9 +9,30 @@ from oscar.apps.checkout.views import PaymentDetailsView as OscarPaymentDetailsV
 Order = get_model('order', 'Order')
 
 class PaymentDetailsView(OscarPaymentDetailsView):
-    '''
-    Deze PaymentDetailsView heeft op dit moment geen payment gateway integratie. Het doel is om een basket in
-    te dienen, waarbij payment niet wordt geautomatiseerd. 
-    '''
+	'''
+	Deze PaymentDetailsView heeft op dit moment geen payment gateway integratie. Het doel is om een basket in
+	te dienen, waarbij payment niet wordt geautomatiseerd. 
+	'''
 
-    pass 
+	def submit(self, user, basket, shipping_address, shipping_method,
+			shipping_charge, billing_address, order_total, payment_kwargs=None, order_kwargs=None):
+
+		self.send_mail(user.email)
+
+
+		return super(PaymentDetailsView, self).submit(user=user, basket=basket, shipping_address=shipping_address,
+			shipping_method=shipping_method, shipping_charge=shipping_charge, billing_address=billing_address,
+			order_total=order_total, payment_kwargs=payment_kwargs, order_kwargs=order_kwargs)
+
+	def send_mail(self, user_email):
+
+		subject = 'Nieuwe bestelling op website!' + ' van: ' + user_email
+		receivers = ['info@tomverheyden.com', 'tim.claes@live.be', ]
+		sender = 'website@tomverheyden.com'
+
+		ctx = {}
+
+		message = get_template('checkout/email/new_order.html').render(Context(ctx))
+		msg = EmailMessage(subject, message, to=receivers, from_email=sender)
+		msg.content_subtype = 'html'
+		msg.send()
